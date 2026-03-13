3ceedfa75153bfa5b7ffb7d3ec72eb26451c2940
Thapelo12345 <79000570+Thapelo12345@users.noreply.github.com>
2026-03-13 14:29:42 +0200

    Fix background video

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ useEffect(()=>{
 }, [Width, Height])
   
   return (
-    // <main className={`relative w-screen ${!sreenRotation ? "h-screen overflow-hidden" : "h-fit overflow-x-hidden overflow-y-auto"} `}>
+    
      <main className="relative flex flex-col p-0 m-0 w-screen min-h-screen overflow-x-hidden">
       <div className="fixed inset-0 -z-20">
       <video
@@ -31,7 +31,8 @@ useEffect(()=>{
         playsInline
         className="w-full h-full object-cover -z-20"
       >
-        <source src={`${import.meta.env.BASE_URL}videos/199285-909903160_medium.mp4" type="video/mp4`} />
+        <source src={`${import.meta.env.BASE_URL}videos/199285-909903160_medium.mp4`}
+          type="video/mp4" />
       </video>
       </div>
       { Width > 768 ? (<NavBar />) : (<MobileNav />)}
